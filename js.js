5756bdd958ef15a53ae44a5899954e951045037f
Ajaybst <97145789+Ajaybst@users.noreply.github.com>
2022-01-20 13:21:57 +0530

    my game

---
+++ js.js
@@ -0,0 +1,67 @@
+function switches() {
+  document.querySelector(`.div-${activePlayer}`).style.background = "black";
+  document.querySelector(`.cscore-${activePlayer}`).textContent = 0;
+  score = 0;
+  if (activePlayer == 0) {
+    activePlayer = 1;
+  } else {
+    activePlayer = 0;
+  }
+  document.querySelector(`.cscore-${activePlayer}`).textContent = score;
+  document.querySelector(`.div-${activePlayer}`).style.background = "green";
+}
+let score = 0;
+let activePlayer = 0;
+let cscore = [0, 0];
+let playing = true;
+document.querySelector(".roll").addEventListener("click", function () {
+  if (playing) {
+    let number = Math.trunc(Math.random() * 5) + 1;
+    document.querySelector(".dice-n").textContent = number;
+    document.querySelector(`.div-${activePlayer}`).style.background = "green";
+    if (number !== 1) {
+      score += number;
+      document.querySelector(`.cscore-${activePlayer}`).textContent = score;
+    } else {
+      switches();
+    }
+  }
+});
+document.querySelector(".hold").addEventListener("click", function () {
+  if (playing) {
+    cscore[activePlayer] += score;
+    document.querySelector(`#hs${activePlayer}`).textContent =
+      cscore[activePlayer];
+    document.querySelector(`.div-${activePlayer}`).style.background = "black";
+    if (cscore[activePlayer] >= 200) {
+      document.querySelector(
+        "#winner"
+      ).textContent = `player-${activePlayer} wins🏆`;
+      document.querySelector(".div").style.background = "green";
+      document.querySelector(`.cscore-${activePlayer}`).background = "black";
+      document.querySelector(".dice-n").textContent = "?";
+      playing = false;
+    }
+    score = 0;
+    document.querySelector(`.cscore-${activePlayer}`).textContent = score;
+
+    if (activePlayer == 0) {
+      activePlayer = 1;
+    } else {
+      activePlayer = 0;
+    }
+  }
+});
+document.querySelector(".try-again").addEventListener("click", function () {
+  document.querySelector("#winner").textContent = `Let's Play`;
+  score = 0;
+  activePlayer = 0;
+  cscore = [0, 0];
+  playing = true;
+  document.querySelector("#hs0").textContent = "0";
+  document.querySelector("#hs1").textContent = "0";
+  document.querySelector(".cscore-0").textContent = "0";
+  document.querySelector(".cscore-1").textContent = "0";
+  document.querySelector(`.div-${activePlayer}`).style.background = "green";
+  document.querySelector("#winner").style.background = `black`;
+});
